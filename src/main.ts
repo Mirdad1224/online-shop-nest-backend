1f75f2ed7afc390809788601598c007d4120c4eb
Mirdad1224 <ali.mirdad@yahoo.com>
2024-09-11 12:21:34 +0330

    chore: use env variables for accessing to the port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT', 5000);
+
+  await app.listen(port);
+  Logger.log(`~ Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

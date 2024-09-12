24a9a214addd955fad9c7bda42c80de3d5437c39
Mirdad1224 <ali.mirdad@yahoo.com>
2024-09-12 10:31:35 +0330

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { corsOptions } from './config/corsOptions';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(corsOptions);
 
   const configService = app.get(ConfigService);
 

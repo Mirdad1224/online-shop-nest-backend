feda81530ffe292e1f5030384e58688c8495a744
Mirdad1224 <ali.mirdad@yahoo.com>
2024-10-01 13:12:23 +0330

    chore: complete env variable interface

--- src/types/IConfig.ts
+++ src/types/IConfig.ts
@@ -3,4 +3,20 @@ export interface ConfigProps {
   PORT: number;
   DB_URI: string;
   DB_NAME: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_REFRESH_SECRET: string;
+  SERVER_URL: string;
+  EMAIL_HOST: string;
+  EMAIL_USERNAME: string;
+  EMAIL_PASSWORD: string;
+  FRONT_URL: string;
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
+  AWS_REGION: string;
+  AWS_S3_BUCKET: string;
+  CLOUDINARY_CLOUD_NAME: string;
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
+  STRIPE_SECRET_KEY: string;
+  ZARINPAL_MERCHANT: string;
 }
